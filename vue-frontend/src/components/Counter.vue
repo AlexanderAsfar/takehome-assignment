7f5bfedd1ad947c7081abb307105ef3757162e6c
AlexanderAsfar <alexasfar99@gmail.com>
2019-09-25 01:24:49 -0400

    Vue part 4 done

--- vue-frontend/src/components/Counter.vue
+++ vue-frontend/src/components/Counter.vue
@@ -1,15 +1,20 @@
 <template>
-  <p>I'm a Counter! <br/>
-    {{count}}<br/>
-    <button v-on:click="count+=1"> Increment </button> <button v-on:click="count-=1"> Decrement </button>
+  <p> 
+    {{count}} <button v-on:click="count+=1"> Increment </button> <button v-on:click="count-=1"> Decrement </button>
   </p>
 </template>
 
 <script>
 export default {
+  props:{
+    init_count: {
+      type: Number,
+      default:0
+      }
+  },
   data() {
     return {
-      count: 0
+      count: this.init_count
     };
   }
 };
